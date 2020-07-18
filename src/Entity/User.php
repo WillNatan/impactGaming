ac03e1b1b1320f60e6a41c8269db8fe5de43db25
WillNatan <willy97438@live.fr>
2020-07-18 11:08:52 +0400

    Inscription et Login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -47,7 +47,7 @@ class User implements UserInterface
     private $lastName;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $Company;
 
