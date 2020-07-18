0ac76d288cccedbf6eaa30c70336059fb2a02481
WillNatan <willy97438@live.fr>
2020-07-18 11:04:06 +0400

    Inscription et Login

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -97,7 +97,7 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        throw new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
